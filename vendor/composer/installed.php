6f564aa8ecc61d10ce84429f618a284eec1d679c
Jack <mrzzcn@gmail.com>
2021-07-26 17:15:49 +0800

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2f91283cc09825b68e5ab28da32b2b51e115fd6e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2f91283cc09825b68e5ab28da32b2b51e115fd6e',
+    ),
+  ),
+);
